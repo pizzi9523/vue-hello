7f80c8d8d7c665cd328445cddcda8ff9c9351872
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-28 15:43:32 +0200

    add image by data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: "#root",
     data: {
-        message: "Hello World"
+        message: "Hello World",
+        image: "https://picsum.photos/id/24/300/300"
     }
 })
